e3566b4739f4f63c41ef8c937c97d795324dcf9f
Praveen07-Codes <praveenvishnu050@gmail.com>
2026-05-20 11:18:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const{app,BrowserWindow}=require('electron');
+function createWindow(){
+    const win=new BrowserWindow({
+        width: 900,
+        height: 700,
+        webpreferences:{
+            nodeIntegration:true,
+            contextIsolation:false
+        }
+    });
+    win.loadFile('index.html');
+}
+app.whenReady().then(createWindow);
